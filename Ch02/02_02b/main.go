35167568c012e483f7623d769704f6f4969e4d3b
thilinanimesh <52148682+thilinanimesh@users.noreply.github.com>
2026-03-04 04:55:38 +0000

    Basic Input and output handling on GO

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,20 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	//When you call the print function you can get two return values such as
+	// Length of the String and the Error for that Println
+	stringLength, err := fmt.Println("The value is", aNumber)
+
+	if err == nil {
+		fmt.Println("String length is:", stringLength)
+	}
+
+	fmt.Printf("Value of number: %v\n", aNumber)
+	fmt.Printf("Data type: %T\n", aNumber)
 }
--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
 
+	// Simple read to get the user input string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Please enter your input ...")
+	str, _ := reader.ReadString('\n') // in Go if you want to ignore the error place an "_" for its name.
+	fmt.Println(str)
+
+	// Simple read to ge the user input int
+
+	fmt.Print("Please enter your number ...")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number:", f)
+	}
 }
